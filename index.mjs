da6b657542e6f91d7e343966f96a981fe1403254
Tiago Neves <53884627+tiago-prog@users.noreply.github.com>
2024-05-23 21:12:04 -0300

    add enviroments variables

--- index.mjs
+++ index.mjs
@@ -1,6 +1,15 @@
 import express from "express";
 import cors from "cors";
 import routes from './src/routes.mjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let PORT;
+
+process.env.STATUS === 'production' 
+    ? (PORT = process.env.PROD_PORT) 
+    : (PORT = process.env.DEV_PORT);
 
 const app = express();
 
@@ -9,5 +18,4 @@ app.use(express.json());
 
 app.use(routes);
 
-
-app.listen(3000);
+app.listen(PORT);
